51db525c6c102db54af6afc98fd7bfb61e22eac7
unknown <igl.myrick@gmail.com>
2026-01-12 15:33:04 -0800

    add Ticket component

---
+++ src/components/Ticket.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Ticket() {
+  const name1 = "Thato";
+  const name2 = "Haley";
+  return (
+    <React.Fragment>
+      <h3>3a</h3>
+      <h3>{name1} and {name2}</h3>
+      <p><em>Firebase entries not saving!</em></p>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Ticket;
