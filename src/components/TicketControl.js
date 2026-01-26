ce52f5cc67f1e1821c2ad2e368c542cc48baf8c1
unknown <igl.myrick@gmail.com>
2026-01-26 15:58:38 -0800

    add new ticket handling

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -4,19 +4,26 @@ import TicketList from './TicketList';
 
 function TicketControl() {
   const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+  const [mainTicketList, setMainTicketList] = useState([]);
 
   const handleClick = () => {
     setFormVisibleOnPage(!formVisibleOnPage);
+  }
+
+  const handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = mainTicketList.concat(newTicket);
+    setMainTicketList(newMainTicketList);
+    setFormVisibleOnPage(false);
   }
 
   let currentlyVisibleState = null;
   let buttonText = null;
 
   if (formVisibleOnPage) {
-    currentlyVisibleState = <NewTicketForm/>;
+    currentlyVisibleState = <NewTicketForm onNewTicketCreation={handleAddingNewTicketToList}/>;
     buttonText = "Return to Ticket List";
   } else {
-    currentlyVisibleState = <TicketList/>;
+    currentlyVisibleState = <TicketList ticketList={mainTicketList}/>;
     buttonText = "Add Ticket";
   }
 
