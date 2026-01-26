8e82102751d26ca6031f15d8d325ccaad1b8034c
unknown <igl.myrick@gmail.com>
2026-01-26 11:51:27 -0800

    add TicketControl component

---
+++ src/components/TicketControl.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+function TicketControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
+
+  return (
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+export default TicketControl;
