99b76ef52cd11e4766fba18c7e7f2b516f46fc37
unknown <igl.myrick@gmail.com>
2026-01-26 16:02:38 -0800

    create new tickets with uuid

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,6 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
 
 function NewTicketForm(props) {
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value,
+      id: v4()
+    });
+  }
+
   return (
     <React.Fragment>
       <form onSubmit={handleNewTicketFormSubmission}>
@@ -19,6 +31,10 @@ function NewTicketForm(props) {
       </form>
     </React.Fragment>
   );
+}
+
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
 }
 
 export default NewTicketForm;
