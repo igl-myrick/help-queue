0b55d853f3f0caf2d2f3438d2d299a3e7db6226b
unknown <igl.myrick@gmail.com>
2026-02-03 15:52:07 -0800

    add new state variables

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import TicketView from './TicketView';
+// import EditTicketForm from './EditTicketForm';
+// import DeleteTicketForm from './DeleteTicketForm';
 
 function TicketControl() {
-  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
+  const [newFormVisible, setNewFormVisible] = useState(false);
+  const [ticketInfoVisible, setTicketInfoVisible] = useState(false);
+  // const [editFormVisible, setEditFormVisible] = useState(false);
+  // const [deleteFormVisible, setDeleteFormVisible] = useState(false);
 
   const handleClick = () => {
-    setFormVisibleOnPage(!formVisibleOnPage);
+    setNewFormVisible(!newFormVisible);
   }
 
   const handleAddingNewTicketToList = (newTicket) => {
     const newMainTicketList = mainTicketList.concat(newTicket);
     setMainTicketList(newMainTicketList);
-    setFormVisibleOnPage(false);
+    setNewFormVisible(false);
   }
 
   let currentlyVisibleState = null;
   let buttonText = null;
 
-  if (formVisibleOnPage) {
+  if (newFormVisible) {
     currentlyVisibleState = <NewTicketForm onNewTicketCreation={handleAddingNewTicketToList}/>;
     buttonText = "Return to Ticket List";
+  } else if (ticketInfoVisible) {
+    currentlyVisibleState = <TicketView/>
+    buttonText = "Return to Ticket List";
+  // } else if (editFormVisible) {
+  //   currentlyVisibleState = <EditTicketForm/>
+  //   buttonText = "Return to Ticket List";
+  // } else if (deleteFormVisible) {
+  //   currentlyVisibleState = <DeleteTicketForm/>
+  //   buttonText = "Return to Ticket List";
   } else {
     currentlyVisibleState = <TicketList ticketList={mainTicketList}/>;
     buttonText = "Add Ticket";
