bd0d6c4821b9c5a5987c754510c8d2ef22a4f7c4
unknown <igl.myrick@gmail.com>
2026-02-18 15:56:17 -0800

    refactor component

--- src/components/TicketView.js
+++ src/components/TicketView.js
@@ -1,22 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 function TicketView(props) {
   return (
     <React.Fragment>
-      <h3>{props.ticket.location} - {props.ticket.names}</h3>
-      <p><em>{props.ticket.issue}</em></p>
-      <button onClick={() => {props.handleEditingTicket()}} type="button">Edit Ticket</button>
-      <button onClick={() => {props.handleDeletingTicket()}} type="button">Delete Ticket</button>
+      <h1>Ticket Details</h1>
+      <p>You clicked on a ticket!</p>
       <hr/>
     </React.Fragment>
-  )
-}
-
-TicketView.propTypes = {
-  ticket: PropTypes.object,
-  handleEditingTicket: PropTypes.func,
-  handleDeletingTicket: PropTypes.func
+  );
 }
 
 export default TicketView;
