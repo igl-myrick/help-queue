8e49e1dc6d0d0b2b2da996b6beff8a78519eca8a
unknown <igl.myrick@gmail.com>
2026-02-18 15:55:30 -0800

    refactor component

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,96 +1,31 @@
 import React, { useState } from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
-import TicketView from './TicketView';
-import EditTicketForm from './EditTicketForm';
-import DeleteTicketForm from './DeleteTicketForm';
 
 function TicketControl() {
+  const [formVisibleOnPage, setFormVisibleOnPage] = useState(false);
   const [mainTicketList, setMainTicketList] = useState([]);
-  const [newFormVisible, setNewFormVisible] = useState(false);
-  const [ticketInfoVisible, setTicketInfoVisible] = useState(false);
-  const [selectedTicket, setSelectedTicket] = useState(null);
-  const [isEditingTicket, setIsEditingTicket] = useState(false);
-  const [isDeletingTicket, setisDeletingTicket] = useState(false);
 
   const handleClick = () => {
-    setNewFormVisible(!newFormVisible);
+    setFormVisibleOnPage(!formVisibleOnPage);
   }
 
   const handleAddingNewTicketToList = (newTicket) => {
     const newMainTicketList = mainTicketList.concat(newTicket);
     setMainTicketList(newMainTicketList);
-    setNewFormVisible(false);
-  }
-  
-  const handleRetrievingTicket = (id) => {
-    let result = null;
-    for (let i = 0; i < mainTicketList.length; i++) {
-      if (mainTicketList[i].id === id) {
-        result = mainTicketList[i];
-        break;
-      }
-    }
-    setSelectedTicket(result);
-    setTicketInfoVisible(true);
-    return result;
-  }
-
-  const beginEdit = () => {
-    setTicketInfoVisible(false);
-    setIsEditingTicket(true);
-  }
-
-  const handleTicketEdit = (ticketToEdit) => {
-    const newMainTicketList = mainTicketList.map(ticket => {
-      if (ticket.id === selectedTicket.id) {
-        return {...ticket, location: ticketToEdit.location, names: ticketToEdit.names, issue: ticketToEdit.issue};
-      }
-    });
-    console.log(newMainTicketList);
-    setMainTicketList(newMainTicketList);
-    setSelectedTicket(ticketToEdit);
-    setIsEditingTicket(false);
-    setTicketInfoVisible(true);
-  }
-
-  const beginDeletion = () => {
-    setTicketInfoVisible(false);
-    setisDeletingTicket(true);
-  }
-
-  const handleTicketDeletion = (ticketToDelete) => {
-    let newMainTicketList = [];
-    for (let i = 0; i < mainTicketList.length; i++) {
-      if (mainTicketList[i].id !== ticketToDelete.id) {
-        newMainTicketList.push(mainTicketList[i]);
-      }
-    }
-    setMainTicketList(newMainTicketList);
+    setFormVisibleOnPage(false);
   }
 
   let currentlyVisibleState = null;
   let buttonText = null;
 
-  if (newFormVisible) {
-    currentlyVisibleState = <NewTicketForm onNewTicketCreation={handleAddingNewTicketToList}/>;
-    buttonText = "Return to Ticket List";
-  } else if (ticketInfoVisible) {
-    currentlyVisibleState = <TicketView 
-      ticket={selectedTicket} 
-      handleEditingTicket={beginEdit}
-      handleDeletingTicket={beginDeletion}/>
-      buttonText = "Return to Ticket List";
-  } else if (isEditingTicket) {
-    currentlyVisibleState = <EditTicketForm onTicketEdit={handleTicketEdit}/>
-    buttonText = "Return to Ticket";
-  } else if (isDeletingTicket) {
-    currentlyVisibleState = <DeleteTicketForm onTicketDeletion={handleTicketDeletion}/>
-    buttonText = "Return to Ticket List";
-  } else {
-    currentlyVisibleState = <TicketList ticketList={mainTicketList} handleRetrievingTicket={handleRetrievingTicket}/>;
-    buttonText = "Add Ticket";
-  }
+  if (formVisibleOnPage) {
+  currentlyVisibleState = <NewTicketForm onNewTicketCreation={handleAddingNewTicketToList}/>;
+  buttonText = "Return to Ticket List";
+} else {
+  currentlyVisibleState = <TicketList ticketList={mainTicketList}/>;
+  buttonText = "Add Ticket";
+}
 
   return (
     <React.Fragment>
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -5,14 +5,12 @@ import PropTypes from "prop-types";
 function TicketList(props) {
   return (
     <React.Fragment>
-      <hr/>
-      {props.ticketList.map((ticket) => 
+      <hr />
+      {props.ticketList.map((ticket) =>
         <Ticket 
-          handleRetrievingTicket={props.handleRetrievingTicket}
           names={ticket.names}
           location={ticket.location}
           issue={ticket.issue}
-          id={ticket.id}
           key={ticket.id}/>
       )}
     </React.Fragment>
@@ -20,8 +18,7 @@ function TicketList(props) {
 }
 
 TicketList.propTypes = {
-  ticketList: PropTypes.array,
-  handleRetrievingTicket: PropTypes.func
-}
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
