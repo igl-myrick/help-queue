dc402ac4ba24da83f83263fe2522abf9e2d119c3
unknown <igl.myrick@gmail.com>
2026-02-19 14:05:04 -0800

    update TicketView to show real ticket data

--- src/components/TicketView.js
+++ src/components/TicketView.js
@@ -1,13 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function TicketView(props) {
+  const { ticket } = props;
+
   return (
     <React.Fragment>
       <h1>Ticket Details</h1>
-      <p>You clicked on a ticket!</p>
+      <h3>{ticket.location} - {ticket.names}</h3>
+      <p><em>{ticket.issue}</em></p>
       <hr/>
     </React.Fragment>
   );
+}
+
+TicketView.propTypes = {
+  ticket: PropTypes.object
 }
 
 export default TicketView;
