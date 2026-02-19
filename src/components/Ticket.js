adc50f03463a8115256f287b46abef42c9648a73
unknown <igl.myrick@gmail.com>
2026-02-19 14:02:38 -0800

    refactor Ticket component

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -4,10 +4,11 @@ import PropTypes from 'prop-types';
 function Ticket(props) {
   return (
     <React.Fragment>
-      <h3>{props.location} - {props.names}</h3>
-      <p><em>{props.issue}</em></p>
-      <button onClick={() => {props.handleRetrievingTicket(props.id)}} type="button">View Ticket</button>
-      <hr/>
+      <div onClick={() => props.whenTicketClicked(props.id)}>
+        <h3>{props.location} - {props.names}</h3>
+        <p><em>{props.issue}</em></p>
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
@@ -16,8 +17,8 @@ Ticket.propTypes = {
   names: PropTypes.string,
   location: PropTypes.string,
   issue: PropTypes.string,
-  key: PropTypes.string,
-  handleRetrievingTicket: PropTypes.func
+  id: PropTypes.string,
+  whenTicketClicked: PropTypes.func
 }
 
 export default Ticket;
