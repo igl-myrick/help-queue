d75af139c8ed077dd1fcec9d83c3a13a19c007b7
unknown <igl.myrick@gmail.com>
2026-02-20 13:25:21 -0800

    rename onEditTicket to onTicketEdit

--- src/components/EditTicketForm.js
+++ src/components/EditTicketForm.js
@@ -3,11 +3,11 @@ import ReusableForm from "./ReusableForm";
 import PropTypes from "prop-types";
 
 function EditTicketForm(props) {
-  const { ticket, onEditTicket } = props;
+  const { ticket, onTicketEdit } = props;
 
   function handleEditTicketFormSubmission(event) {
     event.preventDefault();
-    onEditTicket({
+    onTicketEdit({
       names: event.target.names.value,
       location: event.target.location.value,
       issue: event.target.issue.value,
