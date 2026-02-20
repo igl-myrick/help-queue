c4aa3180298bc038e11ba8261a5549ddeb896cd9
unknown <igl.myrick@gmail.com>
2026-02-20 11:34:30 -0800

    add edit button

--- src/components/TicketView.js
+++ src/components/TicketView.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function TicketView(props) {
-  const { ticket, onClickingDelete } = props;
+  const { ticket, onClickingDelete, onClickingEdit } = props;
 
   return (
     <React.Fragment>
       <h1>Ticket Details</h1>
       <h3>{ticket.location} - {ticket.names}</h3>
       <p><em>{ticket.issue}</em></p>
+      <button onClick={onClickingEdit}>Update Ticket</button>
       <button onClick={() => onClickingDelete(ticket.id)}>Close Ticket</button>
       <hr/>
     </React.Fragment>
@@ -17,7 +18,8 @@ function TicketView(props) {
 
 TicketView.propTypes = {
   ticket: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 }
 
 export default TicketView;
