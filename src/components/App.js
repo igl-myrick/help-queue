3e7780ae77bb123bbdbc2057ad910e3193e77b3e
unknown <igl.myrick@gmail.com>
2026-08-14 14:42:20 -0700

    add toggleTheme logic and context provider/consumer

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import TicketControl from './TicketControl';
 import SignIn from "./SignIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ToggleTheme from './ToggleTheme';
+import { ThemeContext, themes } from '../context/theme-context';
 
 function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  document.body.style.backgroundColor = theme.backgroundColor;
+  document.body.style.color = theme.textColor;
+
+  const toggleTheme = () => {
+    setTheme(theme =>
+      theme.textColor === "AntiqueWhite" ? themes.light : themes.dark
+    );
+  }
+
   return (
-    <Router>
-      <Header/>
-      <ToggleTheme/>
-      <Routes>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/" element={<TicketControl/>}/>
-      </Routes>
-    </Router>
+    <ThemeContext.Provider value={theme}>
+      <Router>
+        <Header/>
+        <ThemeContext.Consumer>
+          {contextTheme => <ToggleTheme theme={contextTheme} toggleTheme={toggleTheme}/>}
+        </ThemeContext.Consumer>
+        <Routes>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/" element={<TicketControl/>}/>
+        </Routes>
+      </Router>
+    </ThemeContext.Provider>
   );
 }
 
