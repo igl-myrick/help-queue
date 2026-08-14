ad844815654db0d2e9cc0417f1089c92f579bc6b
unknown <igl.myrick@gmail.com>
2026-08-14 15:25:59 -0700

    add light/dark theme styling with useContext

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from "../context/theme-context";
 import PropTypes from "prop-types";
 
 function ReusableForm(props) {
   const { formSubmissionHandler, buttonText } = props;
 
+  const theme = useContext(ThemeContext);
+
+  const buttonStyles = {
+    backgroundColor: theme.buttonBackground,
+    color: theme.textColor
+  }
+
+  const inputStyles = {
+    backgroundColor: theme.inputBackground,
+    color: theme.textColor
+  }
+
   return (
     <React.Fragment>
       <form onSubmit={formSubmissionHandler}>
         <input
+          style={inputStyles}
           type="text"
           name="names"
           placeholder="Pair Names"/>
         <input
+          style={inputStyles}
           type="text"
           name="location"
           placeholder="Location"/>
         <textarea
+          style={inputStyles}
           name="issue"
           placeholder="Describe your issue."/>
-        <button type="submit">{buttonText}</button>
+        <button style={buttonStyles} type="submit">{buttonText}</button>
       </form>
     </React.Fragment>
   );
